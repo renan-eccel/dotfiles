93b406b992bb2f04c5e383a9fa946bcf3679b102
Renan Eccel <renan.eccel@wplex.com.br>
2019-09-20 16:51:57 -0300

    change Caps Lock and Esc

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim inputrc tmux.conf tmux zshrc antigen"    # list of files/folders to symlink in homedir
+files="vimrc vim inputrc tmux.conf tmux zshrc antigen speedswapper"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -29,3 +29,6 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# start speedswaper
+xmodmap ~/.speedswapper
